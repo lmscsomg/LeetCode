57fa22e2b1bda9f92d68fe431f72917f4fa88405
lms <lmscsomg@gmail.com>
2015-03-19 00:59:44 +0800

    add longest-substring-without-repearting-characters.cc

---
+++ longest-substring-without-repeating-characters.cc
@@ -0,0 +1,22 @@
+class Solution{
+    public:
+        int lengthOfLongestSubstring(string s){
+            const int MAX_CHARS;
+            int pos[MAX_CAHRS];
+            memset(pos, -1, sizeof(pos));
+
+            //last: the last position of the repeating char
+            //pos: the position of the current char
+            int last = -1, result = 0;
+            for(int i = 0; i < s.size(); i++){
+                char c = s[i];
+                if(pos[c] > last){
+                    last = pos[c];
+                }
+                pos[c] = i;
+                result = max(result, i - last);
+            }
+            return result;
+        }
+};
+
