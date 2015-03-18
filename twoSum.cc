bc40916837e6296a13b7b751aff820739441e874
lms <lmscsomg@gmail.com>
2015-03-18 20:18:42 +0800

    add twoSum.cc

---
+++ twoSum.cc
@@ -0,0 +1,18 @@
+class Solution{
+    public:
+        vector<int> twoSum(vector<int> &numbers, int target){
+            vector<int> result;
+            unordered_map<int> valToIndex;
+
+            for(int i = 0; i < numbers.size(); i++){
+                if(valToIndex.find(numbers[i]) == valToIndex.end()){
+                    valToIndex[target-numbers[i]] = i;
+                }else{
+                    result.push_back(valToIndex[numbers[i]]+1);
+                    result.push_back(i+1);
+                    break;
+                }
+            }
+            return result;
+        }
+};
