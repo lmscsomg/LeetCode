5c50d62bbf9f5296b97371ba8df5b896cf262163
lms <lmscsomg@gmail.com>
2015-03-21 16:20:58 +0800

    solve No.6

--- zigzag-conversion.cc
+++ zigzag-conversion.cc
@@ -3,7 +3,7 @@ class Solution{
         string convert(string s, int nRows){
             //pay attention to the specific situation
             const int n = s.size();
-            if(n<=nRows || nRow ==1)
+            if(n<=nRows || nRows ==1)
                 return s;
 
             const int gap = (nRows-1)*2;
@@ -12,8 +12,8 @@ class Solution{
             for(int i = 0; i < nRows; i++)
                 for(int j = i; j < n; j+=gap){
                     result.push_back(s[j]);
-                    if(0 < j && j < nRows-1 && j+gap-2*i < n)
-                        result.pusht_back(s[j+gap-2*i]);
+                    if(0 < i && i < nRows-1 && j+gap-2*i < n)
+                        result.push_back(s[j+gap-2*i]);
                 }
             return result;
         }
