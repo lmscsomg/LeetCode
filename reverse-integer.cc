0e93e5827105aed43c5689c39377f94919ac1e6a
lms <lmscsomg@gmail.com>
2015-03-21 16:34:17 +0800

    solve No.7

--- reverse-integer.cc
+++ reverse-integer.cc
@@ -2,14 +2,14 @@ class Solution{
     public:
         int reverse(int x){
             if(x < 0){
-                return x < INT_MIN ? 0 : -reverse(-x);
+                return x ==  INT_MIN ? 0 : -reverse(-x);
             }
 
             int result = 0;
             while(x > 0){
                 if(result > INT_MAX/10)
                     return 0;
-                if(return == INT_MAX/10 && (x%10) > 7)
+                if(result == INT_MAX/10 && (x%10) > 7)
                     return 0;
 
                 result = result*10 + x%10;
