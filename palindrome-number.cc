9926e87321da10d3da617a0fcf3f781ddcf9ad1e
lms <lmscsomg@gmail.com>
2015-03-26 21:03:42 +0800

    solve a new problem

--- palindrome-number.cc
+++ palindrome-number.cc
@@ -1,5 +1,24 @@
 class Solution{
     public:
         bool isPalindrome(int x){
+            if(x < 0)
+                return false;
+            if(x < 10)
+                return true;
+
+            int fac = 1;
+            while(x/10 >= fac)
+                fac *= 10;
+
+            while(fac >= 10){
+                int high = x/fac;
+                int low = x%10;
+                if(high != low)
+                    return false;
+                x = (x%fac)/10;
+                fac /= 100;
+            }
+
+            return true;
         }
-}
+};
