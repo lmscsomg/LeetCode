bd4098d47a28836ff00c36f683855acac9902328
lms <lmscsomg@gmail.com>
2015-03-26 21:26:46 +0800

    solve a problem

---
+++ container-with-most-water.cc
@@ -0,0 +1,23 @@
+class Solutoion{
+    public:
+        int maxArea(vector<int> &height){
+            int result = 0;
+
+            int left = 0, right = height.size()-1;
+            while(left < right){
+                int tempArea = 0;
+                if(height[left] <= height[right]){
+                    tempArea = (right - left)*height[left];
+                    left++;
+                }else{
+                    tempArea = (right - left)*height[right];
+                    right--;
+                }
+
+                if(tempArea > result)
+                    result = tempArea;
+            }
+
+            return result;
+        }
+};
